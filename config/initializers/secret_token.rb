23978719f50e5cbc7545042c5fcd5c8b7288dc6e
allenwlee <allenwlee@Allens-MacBook-Pro.local>
2014-07-12 10:06:26 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Allenwlee::Application.config.secret_key_base = '01c7b51c04eeb5f783b5b761b307cab90293498c76742f81f7d7c369d0393c1e10a6d3a66ebca18951bd25de2dc0b3150e1396c2cb4974897cbe9705d5695ef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Allenwlee::Application.config.session_store :cookie_store, key: '_allenwlee_session'
